bf16efa9b96b844b2b16f13b7c977daec3df1ca4
Zac Finger <zac@zacfinger.com>
2020-01-03 15:26:30 -0700

    Now extracting job title from HTML

--- main.py
+++ main.py
@@ -22,38 +22,45 @@ url = 'https://www.indeed.com/jobs?q='+query+'&l='+location
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 
-print(soup)
+# Find all class matching 'jobsearch-SerpJobCard unifiedRow row result clickcard'
+mydivs = soup.findAll("a", {"class": "jobtitle turnstileLink"})
 
-# https://stackoverflow.com/questions/17759860/python-2-smtpserverdisconnected-connection-unexpectedly-closed/33121151
-# Create message container - the correct MIME type is multipart/alternative.
-msg = MIMEMultipart('alternative')
-msg['Subject'] = "Daily Job Report: " + query + ", " + location
-msg['From'] = credentials.recipient
-msg['To'] = credentials.sender
+#print(soup)
 
-# Create the body of the message (a plain-text and an HTML version).
-html = '<html><body><p>Hi, I have the following alerts for you!</p></body></html>'
+for div in mydivs:
+	print(div['title'])
 
-# Record the MIME types of both parts - text/plain and text/html.
-part2 = MIMEText(html, 'html')
+def send_email():
+	# https://stackoverflow.com/questions/17759860/python-2-smtpserverdisconnected-connection-unexpectedly-closed/33121151
+	# Create message container - the correct MIME type is multipart/alternative.
+	msg = MIMEMultipart('alternative')
+	msg['Subject'] = "Daily Job Report: " + query + ", " + location
+	msg['From'] = credentials.recipient
+	msg['To'] = credentials.sender
 
-# Attach parts into message container.
-# According to RFC 2046, the last part of a multipart message, in this case
-# the HTML message, is best and preferred.
-msg.attach(part2)
+	# Create the body of the message (a plain-text and an HTML version).
+	html = soup
 
-# Send the message via local SMTP server.
-s = smtplib.SMTP_SSL('smtp.gmail.com')
-# https://stackoverflow.com/questions/55800902/smtplib-smtpnotsupportederror-smtp-auth-extension-not-supported-by-server
-# https://stackoverflow.com/questions/16512592/login-credentials-not-working-with-gmail-smtp
-s.ehlo()
+	# Record the MIME types of both parts - text/plain and text/html.
+	part2 = MIMEText(html, 'html')
 
-# do the smtp auth; sends ehlo if it hasn't been sent already
-s.login(credentials.sender, credentials.password)
+	# Attach parts into message container.
+	# According to RFC 2046, the last part of a multipart message, in this case
+	# the HTML message, is best and preferred.
+	msg.attach(part2)
 
-# sendmail function takes 3 arguments: sender's address, recipient's address
-# and message to send - here it is sent as one string.
-s.sendmail(credentials.sender, credentials.recipient, msg.as_string())
-s.quit()
+	# Send the message via local SMTP server.
+	s = smtplib.SMTP_SSL('smtp.gmail.com')
+	# https://stackoverflow.com/questions/55800902/smtplib-smtpnotsupportederror-smtp-auth-extension-not-supported-by-server
+	# https://stackoverflow.com/questions/16512592/login-credentials-not-working-with-gmail-smtp
+	s.ehlo()
 
-print("Success")
+	# do the smtp auth; sends ehlo if it hasn't been sent already
+	s.login(credentials.sender, credentials.password)
+
+	# sendmail function takes 3 arguments: sender's address, recipient's address
+	# and message to send - here it is sent as one string.
+	s.sendmail(credentials.sender, credentials.recipient, msg.as_string())
+	s.quit()
+
+print("\nSuccess")
