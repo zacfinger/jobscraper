eede819a5ca7ecc1d24833e5e41f9ed558d3a398
Zac Finger <zac@zacfinger.com>
2020-05-10 16:05:12 -0700

    Slack bot functionality added

---
+++ slackbot.py
@@ -0,0 +1,14 @@
+# https://github.com/slackapi/python-slackclient/wiki/Migrating-to-2.x
+
+from slack import WebClient
+import credentials
+
+client = WebClient(credentials.slack_bot_token, timeout=30)
+client.api_call('chat.postMessage', json={
+    'channel': 'general',
+    'text': 'This is a test!'})
+
+# Note: That while the above is allowed, the more efficient way to call that API is like this:
+"""client.chat_postMessage(
+    channel='C0123456',
+    text='Hi!')"""
