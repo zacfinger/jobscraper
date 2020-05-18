943cb3d2b6aef007383cd39e0ce46b35afbda936
Zac Finger <zac@zacfinger.com>
2020-05-17 20:46:18 -0700

    Corrected token initialization on web client in app.py and added documentation

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 # https://github.com/slackapi/python-slackclient
+# https://stackoverflow.com/questions/52872580/how-to-respond-with-the-correct-challenge-value-when-trying-to-enable-event-subs
+# https://api.slack.com/apps/A013LCCV3QR/event-subscriptions?
+# https://api.slack.com/events/url_verification
+# http://www.islandtechph.com/2017/10/21/how-to-deploy-a-flask-python-2-7-application-on-a-live-ubuntu-16-04-linux-server-running-apache2/
+# http://www.islandtechph.com/2017/10/23/how-to-deploy-a-flask-python-3-5-application-on-a-live-ubuntu-16-04-linux-server-running-apache2/
+# https://stackoverflow.com/questions/46463199/no-such-file-or-directory-mod-wsgi-unable-to-connect-to-wsgi-daemon-process
+# https://flask.palletsprojects.com/en/1.1.x/deploying/mod_wsgi/
 
 # import os
 import logging
@@ -15,7 +22,7 @@ app = Flask(__name__)
 slack_events_adapter = SlackEventAdapter(credentials.signing_secret, "/slack/events", app)
 
 # Initialize a Web API client
-slack_web_client = credentials.slack_bot_token
+slack_web_client = WebClient(token=credentials.slack_bot_token)
 
 #{"channel": {"user_id": OnboardingTutorial}}
 
